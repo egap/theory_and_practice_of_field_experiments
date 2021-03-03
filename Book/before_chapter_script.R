70c001358a247d9358487a1930e15cc55bab4068
jwbowers <jwbowers@users.noreply.github.com>
2021-03-03 22:41:09 +0000

    Deploying to gh-pages from  @ 1a936e9592248ca09c764ec607168ba407908790 🚀

--- Book/before_chapter_script.R
+++
@@ -1,8 +0,0 @@
-## Currently empty.
-
-#require(Hmisc)    # provides knitrSet and other functions
-#knitrSet(lang='markdown', fig.path='png/', fig.align='left', w=6.5, h=4.5, cache=FALSE)
-# If using blogdown: knitrSet(lang='blogdown')
-
-#`r hidingTOC(buttonLabel="Outline")`
-
